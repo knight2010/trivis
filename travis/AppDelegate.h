f97a4c280a60164d682eed8f688f436bc43128df
kshopping <k-night@163.com>
2016-04-20 11:03:39 +0800

    Copyright © 2016, go-swift.net

---
+++ travis/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  travis
+//
+//  Created by knight on 16/4/20.
+//  Copyright © 2016年 go-swift.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ travis/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  travis
+//
+//  Created by knight on 16/4/20.
+//  Copyright © 2016年 go-swift.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
